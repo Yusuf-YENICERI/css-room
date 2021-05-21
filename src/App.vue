2349987bb0322d26e7a98fc6040cacc2fb9cc934
WoxeOfficial <57907566+WoxeOfficial@users.noreply.github.com>
2021-05-21 21:52:03 +0300

    deploy commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="room">
+    <div class="roomSkeleton">
+      <Square width="500" height="500"
+              rotatex="0" rotatey="0" rotatez="0"
+              translatex="-125" translatey="0" translatez="-125"
+              bg="linear-gradient(to right, #eee, #888)" thickness="20"></Square>
+      <Square width="500" height="500"
+              rotatex="180" rotatey="-90" rotatez="0"
+              translatex="-125" translatey="0" translatez="-125"
+              bg="linear-gradient(to right, #eee, #888)" thickness="20"></Square>
+      <Square width="520" height="520"
+              rotatex="90" rotatey="0" rotatez="0"
+              translatex="-115" translatey="115" translatez="-250"
+              bg="linear-gradient(to top right, #26110D, #120806)" thickness="20"></Square>
+      <!--
+      for floor :
+        width & height =  wallWidth + thickness
+        translateX & translateY = wallWidth / 4 - thickness / 2
+      -->
+    </div>
+    <div class="roomItems">
+      <div class="desk">
+        <Square width="300" height="16"
+                rotatex="0" rotatey="90" rotatez="0"
+                translatex="-125" translatey="100" translatez="125"
+                bg="linear-gradient(to bottom left, black, #222)" thickness="125"></Square>
+        <Square width="16" height="150"
+                rotatex="0" rotatey="0" rotatez="0"
+                translatex="8" translatey="175" translatez="0"
+                bg="linear-gradient(to top right, black, #222)" thickness="16"></Square>
+        <Square width="16" height="150"
+                rotatex="0" rotatey="0" rotatez="0"
+                translatex="8" translatey="175" translatez="268"
+                bg="linear-gradient(to top right, black, #222)" thickness="16"></Square>
+      </div>
+      <span class="wallText">WoxeOfficial</span>
+      <div class="computer">
+        <div class="keyboard">
+          <Square width="80" height="4"
+                  rotatex="0" rotatey="0" rotatez="0"
+                  translatex="50" translatey="90" translatez="174"
+                  bg="gray" thickness="90"></Square>
+
+          <Square width="70" height="1"
+                  rotatex="0" rotatey="0" rotatez="0"
+                  translatex="50" translatey="88" translatez="169"
+                  bg="darkgray" thickness="80"></Square>
+        </div>
+
+        <div class="screen">
+          <Square width="80" height="4"
+                  rotatex="0" rotatey="0" rotatez="0"
+                  translatex="50" translatey="90" translatez="174"
+                  bg="gray" thickness="90"></Square>
+
+          <Square width="70" height="1"
+                  rotatex="0" rotatey="0" rotatez="0"
+                  translatex="50" translatey="88" translatez="169"
+                  bg="#1D1D1DFF" thickness="80"></Square>
+        </div>
+      </div>
+      <div class="wallLogo">
+
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Square from "./components/Square"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Square,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Square.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+      class="square"
+      :style="`--width: ${width}px; --height: ${height}px; --bg: ${bg}; --rotateX: ${rotatex}deg; --rotateY: ${rotatey}deg; --rotateZ: ${rotatez}deg; --translateX: ${translatex}px; --translateY: ${translatey}px; --translateZ: ${translatez}px`"
+  >
+    <div class="front side"></div>
+    <div :style="`--thickness: ${-thickness}px;`"
+         class="back side"></div>
+    <div class="left side" :style="`--thickness: ${thickness}px; --width: ${-width / 2 + 1}px;`"></div>
+    <div class="right side" :style="`--thickness: ${thickness}px; --width: ${-width / 2 + 1}px;`"></div>
+    <div class="top side" :style="`--thickness: ${thickness}px; --width: ${width}px; --translate: ${height / 2}px`"></div>
+    <div class="bottom side" :style="`--thickness: ${thickness}px; --width: ${width}px; --translate: ${height / 2}px`"></div>
+
+    <!-- 1px for remove little space between sides -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Square',
+  props: ['rotatex', 'rotatey', 'rotatez', 'translatex', 'translatey', 'translatez', 'width', 'height', "bg", "thickness"],
+};
+</script>
